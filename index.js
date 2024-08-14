df1c166dbe7b7890f92a8a2348e4fd07ee6a884d
Prince Pandey <opuniqueprince@gmail.com>
2024-08-14 05:06:37 +0000

    Created SERVER which is running and up on PORT number 3000

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// app.use('/api', require('./routes/api.route')(db));
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
